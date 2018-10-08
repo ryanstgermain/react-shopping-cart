d2bad224e77a98c74c46a54d8975d80f52bc212d
ryanstgermain <ryanjoel26@gmail.com>
2018-10-08 16:12:58 -0600

    cleaned up JS/CSS, installed bootstrap, added a components folder, imported ccs and js

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import '../style/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App"></div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/cart_header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import './style/index.css';
+
+
+const Greeting = () => {
+  render(){
+  
+  }
+  return (
+    <h1>Hello!</h1>
+  )
+}
+
+export default Greeting
