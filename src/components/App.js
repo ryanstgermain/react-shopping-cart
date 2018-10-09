2d375f67d0656d0bde6d10cf8894632baf4483c5
ryanstgermain <ryanjoel26@gmail.com>
2018-10-09 09:54:12 -0600

    added footer and fixed a few things

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,18 @@
 import React, { Component } from 'react';
+import CartFooter from './CartFooter';
 import '../style/App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <nav className="navbar navbar-dark bg-primary">
-        <a className="navbar-brand" href="#">Shopping Cart</a>
-      </nav>
-    );
-  }
+    render() {
+        return (
+            <div>
+            <nav className="navbar navbar-dark bg-primary">
+            <a className="navbar-brand" href="#">Shopping Cart</a>
+            </nav>
+            <CartFooter />
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/components/CartFooter.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../style/index.css';
+
+class CartFooter extends Component {
+    render() {
+    return (
+        <nav className="navbar navbar-dark bg-dark">
+            <a className="navbar-brand" href="#">&copy; 2018</a>
+        </nav>
+        );
+    }
+}
+
+export default CartFooter;
