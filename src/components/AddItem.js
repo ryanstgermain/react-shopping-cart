40d91f109a7163ec0bd022c0df3c7672925730f6
ryanstgermain <ryanjoel26@gmail.com>
2018-10-22 15:58:42 -0600

    added a quantity section

--- src/components/AddItem.js
+++ src/components/AddItem.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import '../style/index.css';
 
-const AddItem = (props) => {
+const AddCartItem = (props) => {
     const AddedItem = props.products.map(product => {
         return (
-        
+            <option>{product.name} - {product.priceInCents}</option>
         )
     })
-        return (
-            <div className="collection-item">{AddedItem}</div>
-        )
+    return (
+            <div className="form-group">
+                <label for="sel1">Select list:</label>
+                <select className="form-control" id="sel1">
+                    {AddedItem}
+                </select>
+                <input className="quantityBox" type="number" placeholder="Quantity"/>
+            </div>
+    )
 }
 
-export default AddItem;
+export default AddCartItem;
